478ca8ec2a37208e232148bd9941fe64b689e278
MerlinHose <hoeslmilan6@gmail.com>
2025-04-04 13:29:29 +0200

    Default

---
+++ REPOModTemplate/Patches/PlayerControllerPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace REPOModTemplate.Patches
+{
+    [HarmonyPatch(typeof(PlayerController))]
+    public class PlayerControllerPatch
+    {
+        [HarmonyPatch("CrouchSpeed")]
+        [HarmonyPrefix]
+        static void CrouchSpeedPatch(ref float result)
+        {
+            result = 3f;
+        }
+    }
+}
---
+++ REPOModTemplate/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using REPOModTemplate.Patches;
+
+namespace REPOModTemplate
+{
+    [BepInPlugin(Guid, Name, Version)]
+    public class RepoModClass : BaseUnityPlugin
+    {
+        private const string Guid = "com.yourname.repomodtemplate";
+        private const string Name = "REPO Mod Template";
+        private const string Version = "1.0.0";
+        
+        private readonly Harmony _harmony = new Harmony(Guid);
+        
+        private static RepoModClass _instance;
+        private ManualLogSource _logger;
+
+        private void Awake()
+        {
+            _logger = BepInEx.Logging.Logger.CreateLogSource(Guid);
+            
+            if (_instance == null)
+            {
+                _instance = this;
+                _harmony.PatchAll(typeof(RepoModClass));
+                _harmony.PatchAll(typeof(PlayerControllerPatch));
+                
+                _logger.LogInfo($"Plugin {Name} is loaded!");
+            }
+            else
+            {
+                _logger.LogWarning($"Plugin {Name} is already loaded!");
+            }
+        }
+    }
+}
